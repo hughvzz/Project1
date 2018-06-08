8365350cf5a59326b470df077f74bb197eadc487
HugoVzz <hughvzz@gmail.com>
2018-06-07 20:07:31 -0500

    new function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
 
-    $("#picTwitter").click(function() {
-        $(".detailsCard").toggle( "slow", function() {
-           
-          });
+    $("#picTwitter").click(function () {
+        $(".detailsCard").toggle("slow", function () {
+
+        });
     });
+
+    $(document).ready(function () {
+
+        $("#picDanger").click(function () {
+            $(".detailsCard2").toggle("slow", function () {
+
+            });
+        });
     
+    });
 });
